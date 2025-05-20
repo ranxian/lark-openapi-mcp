e0d8799b064cef48b6e7fe3b827000a4e2fd1275
chenli.idevlab <chenli.idevlab@bytedance.com>
2025-05-20 19:33:03 +0800

    fix: cli args default value merge

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,22 @@
 import { currentVersion } from './version';
 
 export const USER_AGENT = `oapi-sdk-mcp/${currentVersion}`;
+
+export const OAPI_MCP_DEFAULT_ARGS = {
+  domain: 'https://open.feishu.cn',
+  toolNameCase: 'snake',
+  language: 'en',
+  tokenMode: 'auto',
+  mode: 'stdio',
+  host: 'localhost',
+  port: '3000',
+};
+
+export const OAPI_MCP_ENV_ARGS = {
+  appId: process.env.APP_ID,
+  appSecret: process.env.APP_SECRET,
+  userAccessToken: process.env.USER_ACCESS_TOKEN,
+  tokenMode: process.env.LARK_TOKEN_MODE,
+  tools: process.env.LARK_TOOLS,
+  domain: process.env.LARK_DOMAIN,
+};
